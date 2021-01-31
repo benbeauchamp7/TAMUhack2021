c71a067d6c3db4aa6582e65dc2377adfeabb4e5f
Benjamin Beauchamp <benbeauchamp7@tamu.edu>
2021-01-31 10:06:59 -0600

    TAMUhack2021 rem files

--- Test.py
+++
@@ -1,6 +0,0 @@
-v = "$AAL $ACB $AG $AMC $AMD $BB $BBBY $BYND $CCIV $CLOV $CRIS $GM $GME $HIMS $INO $IPOE $IPOF $KOSS $MRNA $NCTY $NVAX"
-tickers = ["$MSR", "$AMC"]
-for word in v.split(' '):
-  for t in tickers:
-    if word.lower() == t.lower():
-      print(word)
